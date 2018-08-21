a6a5feacf25aeac16f4634750a91508fa32d0792
zhangmingyuan <zhangmingyuan@youzan.com>
2018-08-21 21:14:02 +0800

    feat: graphql

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 //app.js
+const wxgql = require('./graphql/wxgpl');
+
+const gql = wxgql.GraphQL({
+  url: 'http://127.0.0.1:8099/gp' // url必填 
+});
+
+
 App({
   onLaunch: function () {
     // 展示本地存储能力
@@ -9,6 +16,26 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log(res)
+        gql({
+          body:{
+            query:`
+              query ($id: String!) {
+                account(id: $id) {
+                  name
+                  assets {
+                    name
+                    amount
+                    id
+                  }
+                }
+              }
+            `,
+            variables: {
+              id: '5b743137aed15d12cd7895b0'
+            }
+          }
+        })
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
